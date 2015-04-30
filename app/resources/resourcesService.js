f245ffd59ac2b976ec7c6b27202bf119a7c18867
ush189 <github@ush189.de>
2015-04-30 21:39:29 +0200

    added TODO

--- app/resources/resourcesService.js
+++ app/resources/resourcesService.js
@@ -1,5 +1,6 @@
 app.factory('ResourcesService', ['$http', function($http) {
     var getPrices = function() {
+        // TODO get real data per node.js
         return $http.get('mocks/kurseliste_json.txt');
     };
 
