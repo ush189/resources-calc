47795e01796ae22e491675392dbe98a8c22b352a
ush189 <github@ush189.de>
2015-05-16 22:54:48 +0200

    request prices from heroku node app

--- app/resources/resourcesService.js
+++ app/resources/resourcesService.js
@@ -1,6 +1,7 @@
 app.factory('ResourcesService', ['$http', function($http) {
     var getPrices = function() {
-        return $http.get('http://localhost:3000/prices');
+        // TODO caching
+        return $http.get('https://resources-fetch.herokuapp.com/prices');
     };
 
     return {
