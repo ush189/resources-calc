3e0dcf7018f17a429a3887959903a7e6d3ee0a6a
ush189 <github@ush189.de>
2015-06-06 18:21:24 +0200

    get name of factory product

--- app/factories/factoriesController.js
+++ app/factories/factoriesController.js
@@ -1,12 +1,23 @@
-app.controller('FactoriesController', ['$scope', 'FactoriesService', function($scope, FactoriesService) {
+app.controller('FactoriesController', ['$scope', 'FactoriesService', 'ResourcesService', function($scope, FactoriesService, ResourcesService) {
      FactoriesService.getFactories().then(
         function(response) {
             $scope.factories = response.data;
             console.log(response.data);
-
         },
         function(error) {
-
+            console.log('error', error);
         }
     );
+
+    $scope.product = {};
+    ResourcesService.getPrices().then(
+        function(response) {
+            _.forEach(response.data, function(item) {
+                $scope.product[item.ITEM_ID] = item.NAME_DE;
+            });
+        },
+        function(error) {
+            console.log('error', error);
+        }
+    )
 }]);
